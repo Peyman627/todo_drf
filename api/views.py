7b93f4aebdb7391b338f90f138c06254fdbae198
Peyman Hassani <hassani.peyman627@gmail.com>
2021-02-26 15:48:00 +0330

    list and detail view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
+from django.db.models import manager
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Task
+from .serializers import TaskSerializer
 
 
 @api_view(['GET'])
@@ -12,3 +16,17 @@ def api_overview(request):
         'Delete': '/task-delete/<str:pk>/',
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def task_detail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
